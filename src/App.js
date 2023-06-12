201c4ab65817e7d76e64b00254894d8b9a913b1c
Lily <aulily09@gmail.com>
2023-06-12 14:59:57 -0700

    Create AnimalList component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Animal from './components/Animal';
+import AnimalList from './components/AnimalList';
 
 function App() {
   return (
@@ -9,11 +10,8 @@ function App() {
       <h2>Animal Listings</h2>
       {/* I want to list a bunch of animals... */}
       {/* Each animal has its name and photo */}
+      <AnimalList></AnimalList>
       <Animal></Animal>
-      <Animal></Animal>
-      <Animal></Animal>
-      <Animal></Animal>
-
     </section>
   );
 }
