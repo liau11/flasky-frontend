b5b7130967e3f8b41e913a9ee82964890fea3b1f
Lily <aulily09@gmail.com>
2023-06-12 15:03:47 -0700

    Moved component to appropriate file

--- src/App.js
+++ src/App.js
@@ -7,11 +7,9 @@ function App() {
   return (
     <section>
       <h1>The Sapphire Animal Adoption Agency</h1>
-      <h2>Animal Listings</h2>
       {/* I want to list a bunch of animals... */}
       {/* Each animal has its name and photo */}
       <AnimalList></AnimalList>
-      <Animal></Animal>
     </section>
   );
 }
