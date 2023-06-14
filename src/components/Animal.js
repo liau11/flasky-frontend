51fe733f8fbce4e82bdd3a291a78e82e11796f14
Lily <aulily09@gmail.com>
2023-06-14 11:01:12 -0700

    Added component and props

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -1,11 +1,11 @@
 import './Animal.css'
 
-// The responsibility of this component is to be a reusable UI element that displays and Animal's name, species and photo
-const Animal = () => {
+// The responsibility of this component is to be a reusable UI element that displays and Animal's name, species, age, and photo (optional? default photo?)
+const Animal = (props) => {
     return (<section className="Animal">
-        <h3>Animal name: Willow</h3>
-        <p>Species: cat</p>
-        <img src="http://placekitten.com/g/200/300" alt="Photo of Willow">
+        <h3>Animal name: {props.name} </h3>
+        <p>Species: {props.species}</p>
+        <img src="http://placekitten.com/g/200/300" alt="Willow the cat">
         </img>
     </section>)
 };
--- src/components/AnimalList.js
+++ src/components/AnimalList.js
@@ -2,12 +2,29 @@ import './AnimalList.css';
 import Animal from './Animal';
 
 const AnimalList = () => {
+    const firstAnimal = {
+        name: "Violet",
+        species: "pit mix"
+    };
+
+    const secondAnimal = {
+        name: "Cocoa",
+        species: "cat"
+    }
+
+    const animals = [firstAnimal, secondAnimal]
+
     return (<section className="AnimalList">
         <h2>Animal List</h2>
-        <Animal></Animal>
-        <Animal></Animal>
-        <Animal></Animal>
-        <Animal></Animal>
+        <ul className="AnimalList_list">
+            {animals.map(creature => (
+                <li>
+                    <Animal name={creature.name} species={creature.species} />
+                </li>
+            ))
+            }
+        </ul>
+
     </section>)
 };
 
